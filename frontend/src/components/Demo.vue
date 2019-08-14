6543954fb12e6f830ee757faf56f3df75fa97030
Owen Ou <oeg8168@gmail.com>
2019-08-14 18:23:06 +0800

    add frontend Demo basic layout

--- frontend/src/components/Demo.vue
+++ frontend/src/components/Demo.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="demo">
 
+    <b-container fluid>
+      <b-row align-h="center">
+        <p class="text-center">
+          <font-awesome-icon icon="rocket" />&nbsp;DEMO
+        </p>
+      </b-row>
+
+      <b-row align-h="center">
+        <b-col cols="4"
+               offset="1">
+        </b-col>
+
+        <b-col cols="6">
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
