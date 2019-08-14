2443b4f3fb1b342d0bcbb44e872da4a9308b8ed7
Owen Ou <oeg8168@gmail.com>
2019-08-14 10:50:51 +0800

    add frontend necessary packages (axios, boostrap-vue, element-ui, fontawesome, animate.css, v-charts)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/fontawesome'
+import './plugins/bootstrap-vue'
+import './plugins/axios'
 import App from './App.vue'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
