1b9b4e33947d63dcb966d232992ff1584a827ca2
Owen Ou <oeg8168@gmail.com>
2019-08-14 17:07:55 +0800

    add frontend Demo.vue scaffold

---
+++ frontend/src/components/Demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="demo">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Demo",
+  data: function() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
