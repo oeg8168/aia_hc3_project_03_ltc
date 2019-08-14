a6d5ed41e9964fcdcc9d947455043127fbf0df05
Owen Ou <oeg8168@gmail.com>
2019-08-14 17:06:03 +0800

    add frontend i18n package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,12 @@ import './plugins/fontawesome'
 import './plugins/bootstrap-vue'
 import './plugins/axios'
 import App from './App.vue'
+import i18n from './i18n'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
