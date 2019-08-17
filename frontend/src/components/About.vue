367851b502703bb4ac79e47c0d8a0ed7e30849d6
Owen Ou <oeg8168@gmail.com>
2019-08-17 16:18:23 +0800

    implement frontend About.vue

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -1,6 +1,86 @@
 <template>
   <div class="about">
 
+    <h3 class="pl-5 mb-3">{{ $t('PAGE.NAV.About') }}</h3>
+
+    <b-container fluid>
+      <el-divider>{{ $t("PAGE.ABOUT.SUBTITLE.Author") }}</el-divider>
+      <b-row align-h="center"
+             align-v="center"
+             class="mb-5">
+        <b-col v-for="author in authors"
+               :key="author.name"
+               cols="3">
+          <el-card shadow="hover">
+            <div slot="header"
+                 class="author-card-header">
+              <h5 v-if="author.name">{{ author.name }}</h5>
+              <el-link v-if="author.mail"
+                       :href="`mailto:${author.mail}`"
+                       :underline="false">
+                <p>{{ author.mail }}</p>
+              </el-link>
+            </div>
+            <div class="bottom author-card-body">
+              <el-link v-if="author.github"
+                       class="mr-3"
+                       target="_blank"
+                       :href="author.github"
+                       :underline="false">
+                <font-awesome-icon size="2x"
+                                   fixed-width
+                                   :icon="['fab', 'github']"></font-awesome-icon>
+              </el-link>
+              <el-link v-if="author.linkedin"
+                       class="mr-3"
+                       target="_blank"
+                       :href="author.linkedin"
+                       :underline="false">
+                <font-awesome-icon size="2x"
+                                   fixed-width
+                                   :icon="['fab', 'linkedin']"></font-awesome-icon>
+              </el-link>
+            </div>
+          </el-card>
+        </b-col>
+      </b-row>
+
+      <el-divider>{{ $t("PAGE.ABOUT.SUBTITLE.Tech_Stack") }}</el-divider>
+      <b-row align-h="center"
+             align-v="center"
+             class="mb-5">
+        <b-col v-for="tech in techStacks"
+               :key="tech.name"
+               cols="2">
+          <a target="_blank"
+             :href="tech.link">
+            <b-img :src="tech.image"
+                   :alt="tech.name"
+                   :title="tech.name"
+                   class="logo-tech-stack"
+                   center
+                   fluid></b-img>
+          </a>
+        </b-col>
+      </b-row>
+
+      <el-divider>{{ $t("PAGE.ABOUT.SUBTITLE.Special_Thank") }}</el-divider>
+      <b-row align-h="center"
+             align-v="center">
+        <b-col cols="4">
+          <a target="_blank"
+             href="https://aiacademy.tw/">
+            <b-img :src="require('@/assets/logo-aia.png')"
+                   alt="Taiwan AI Academy"
+                   title="Taiwan AI Academy"
+                   class="logo-special-thank"
+                   center
+                   fluid></b-img>
+          </a>
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
@@ -8,11 +88,73 @@
 export default {
   name: "About",
   data: function() {
-    return {};
+    return {
+      authors: [
+        {
+          name: "Ou, Yi-Jing",
+          mail: "oeg8168@gmail.com",
+          github: "https://github.com/oeg8168/",
+          linkedin: "https://www.linkedin.com/in/yi-jing-ou/"
+        },
+        {
+          name: "Chou, Ting-Yu",
+          mail: "asd123975@gmail.com",
+          github: "",
+          linkedin: ""
+        }
+      ],
+
+      techStacks: [
+        {
+          name: "Vue.js",
+          image: require("@/assets/logo-vue.png"),
+          link: "https://vuejs.org/"
+        },
+        {
+          name: "Python",
+          image: require("@/assets/logo-python.png"),
+          link: "https://www.python.org/"
+        },
+        {
+          name: "Flask",
+          image: require("@/assets/logo-flask.png"),
+          link: "https://flask.palletsprojects.com"
+        },
+        {
+          name: "XGBoost",
+          image: require("@/assets/logo-xgboost.png"),
+          link: "https://github.com/dmlc/xgboost"
+        },
+        {
+          name: "App Engine",
+          image: require("@/assets/logo-app_engine.png"),
+          link: "https://cloud.google.com/appengine/"
+        }
+      ]
+    };
   },
   methods: {}
 };
 </script>
 
 <style lang="scss" scoped>
+.author-card {
+  &-header {
+    min-height: 5rem;
+  }
+
+  &-body {
+    min-height: 2rem;
+  }
+}
+
+.logo {
+  &-tech-stack {
+    max-height: 100px;
+  }
+
+  &-special-thank {
+    max-height: 200px;
+  }
+}
 </style>
--- frontend/src/plugins/element.js
+++ frontend/src/plugins/element.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
-import { Form, FormItem, Button, Select, Option, TimePicker, Slider } from 'element-ui'
+import { Card, Divider, Link, Form, FormItem, Button, Select, Option, TimePicker, Slider } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/zh-TW'
 import locale from 'element-ui/lib/locale'
 
 locale.use(lang)
 
+Vue.use(Card)
+Vue.use(Divider)
+Vue.use(Link)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Button)
