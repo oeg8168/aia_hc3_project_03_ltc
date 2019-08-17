b739a6536986ce339316c0226601be2bfd83506c
Owen Ou <oeg8168@gmail.com>
2019-08-17 10:43:32 +0800

    add frontend vue-router supports

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,21 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
+
 import Vue from 'vue'
+import i18n from './i18n'
+import router from './plugins/router.js'
 import './plugins/fontawesome'
 import './plugins/bootstrap-vue'
 import './plugins/axios'
-import App from './App.vue'
-import i18n from './i18n'
 import './plugins/element.js'
-
 import 'animate.css'
+
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   i18n,
+  router,
   render: h => h(App)
 }).$mount('#app')
